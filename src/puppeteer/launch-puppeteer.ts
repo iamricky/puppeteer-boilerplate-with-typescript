8076c6be2558d34aeaa28cee6d2d611785964af3
Ricky <iamricky@users.noreply.github.com>
2023-11-30 01:19:51 -0500

    add puppeteer

---
+++ src/puppeteer/launch-puppeteer.ts
@@ -0,0 +1,52 @@
+import puppeteer from "puppeteer-extra";
+import {
+  type Browser,
+  type BrowserContext,
+  type PuppeteerLaunchOptions,
+  DEFAULT_INTERCEPT_RESOLUTION_PRIORITY,
+} from "puppeteer";
+import AdblockerPlugin from "puppeteer-extra-plugin-adblocker";
+import AnonymizeUserAgentPlugin from "puppeteer-extra-plugin-anonymize-ua";
+import BlockResourcesPlugin from "puppeteer-extra-plugin-block-resources";
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
+import { defaultArgs, preventZombieInstances } from "./args";
+
+puppeteer.use(StealthPlugin());
+puppeteer.use(AnonymizeUserAgentPlugin());
+
+puppeteer.use(
+  BlockResourcesPlugin({
+    blockedTypes: new Set([
+      "image",
+      "other",
+      // 'stylesheet'
+    ]),
+    // Optionally enable Cooperative Mode for several request interceptors
+    interceptResolutionPriority: DEFAULT_INTERCEPT_RESOLUTION_PRIORITY,
+  }),
+);
+
+puppeteer.use(
+  AdblockerPlugin({
+    blockTrackers: true,
+    // Optionally enable Cooperative Mode for several request interceptors
+    interceptResolutionPriority: DEFAULT_INTERCEPT_RESOLUTION_PRIORITY,
+  }),
+);
+
+const puppeteerOptions: PuppeteerLaunchOptions = {
+  // devtools: true,
+  // dumpio: true,
+  // headless: "new",
+  headless: false,
+  ignoreHTTPSErrors: true,
+  args: defaultArgs.concat(preventZombieInstances, [
+    // "--headless"
+  ]),
+};
+
+export async function launchPuppeteer(): Promise<[Browser, BrowserContext]> {
+  const browser = await puppeteer.launch(puppeteerOptions);
+  const browserContext = await browser.defaultBrowserContext();
+  return [browser, browserContext];
+}
